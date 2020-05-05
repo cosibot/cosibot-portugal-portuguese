e5ac8295c6151e60c6e6364bd057b1ec645f311e
martasls <martasls95@gmail.com>
2020-05-05 15:20:41 +0100

    Language detection component done on policy

--- bot/language_detection/lang_change_policy.py
+++ bot/language_detection/lang_change_policy.py
@@ -4,12 +4,16 @@ import logging
 import typing
 from typing import Any, List, Text, Optional
 
+from fasttext import load_model
+import numpy as np
+
 import rasa.utils.io
 
 from rasa.core.domain import Domain #, InvalidDomain
 from rasa.core.policies import Policy
-from rasa.core.actions.action import ACTION_LISTEN_NAME #don't know if needed
+from rasa.core.actions.action import ACTION_LISTEN_NAME
 
+from rasa.core.featurizers import TrackerFeaturizer
 from rasa.core.trackers import DialogueStateTracker
 from rasa.core.constants import FALLBACK_POLICY_PRIORITY
 from rasa.core.events import UserUttered
@@ -21,9 +25,12 @@ class LangChangePolicy(Policy):
 
     def __init__(
         self,
+       # featurizer: Optional[TrackerFeaturizer],
         priority: int = FALLBACK_POLICY_PRIORITY,
+        model = None,
         lang_detect_threshold: float = 0.7,
         fallback_action_name: Text = "action_ask_language",
+        model_path: Text = './language_detection/lid.176.ftz'
     ) -> None:
         """ Create a Language Detection Fallback Policy:
     
@@ -40,46 +47,60 @@ class LangChangePolicy(Policy):
 
         self.lang_detect_threshold = lang_detect_threshold
         self.fallback_action_name = fallback_action_name
+        self.model_path = model_path
+        print(self.model_path)
+        self.model = model or self._default_model(self.model_path)
 
+        # attributes that need to be restored after loading
+        self._pickle_params = ["model"]
+        
         logger.debug(f"Adopted LCP threshold: {self.lang_detect_threshold}")
         logger.debug(f"LCP action to fall out: {self.fallback_action_name}")
     
+    @staticmethod
+    def _default_model(path) -> Any:
+        print(path)
+        return load_model(path)
+
+    @property
+    def _state(self):
+        return {attr: getattr(self, attr) for attr in self._pickle_params}
+        
     def train(
         self,
         training_trackers: List[DialogueStateTracker],
         domain: Domain,
         **kwargs: Any,
     ) -> None:
-        """Does nothing. This policy is deterministic."""
-        pass
-
+        """Loads a pretrained language id fasttext model"""
+        self.model = load_model(self.model_path)
+        print("modelo carregado")
+        
     def predict_action_probabilities(self, tracker, domain):
 
-        def get_value_and_confidence(skip=0):
+        def predict_language(skip=0):
             fetcher = tracker.get_last_event_for(UserUttered, skip=skip)
             if fetcher is not None:
-                entities_list = fetcher.as_dict().get("parse_data").get("entities")
-                for d in entities_list: # d stands for dictionary...
-                    if d['entity'] == 'bot_language_code':
-                        lang_value = d['value']
-                        lang_conf = d['confidence']
-                        value_and_conf = dict(value=lang_value,confidence=lang_conf)
-                        return value_and_conf
+                user_utter = fetcher.as_dict().get("text")
+                language_prediction = self.model.predict(user_utter)
+                print(language_prediction)
+                return language_prediction
 
-        def lang_detect_above_threshold_Q(record):
-            if record and record is not None: 
-                return (record.get('confidence') > self.lang_detect_threshold) 
+        def Q_lang_detect_above_threshold(prediction):
+            if prediction and prediction is not None:
+                return (float(prediction[1]) > self.lang_detect_threshold) 
                     
-        latest_lang_record = get_value_and_confidence()
-        if get_value_and_confidence(skip=1):
-            previous_lang_record = get_value_and_confidence(skip=1)
+        latest_lang_prediction = predict_language()
+        if predict_language(skip=1):
+            previous_lang_prediction = predict_language(skip=1)
         elif tracker.get_slot("language_slot"):
-            previous_lang_record = dict(value=tracker.get_slot("language_slot"), confidence=1.0)
+            language_code = tracker.get_slot("language_slot")
+            previous_lang_prediction = (((f'__label__{language_code}'),), np.array([1.0]))# to have the same type as fasttext model output
         else:
-            previous_lang_record = None
+            previous_lang_prediction = None
         
-        logger.debug(f"language present record is: {latest_lang_record}")
-        logger.debug(f"previous language record is: {previous_lang_record}")
+        logger.debug(f"language present prediction is: {latest_lang_prediction}")
+        logger.debug(f"previous language prediction is: {previous_lang_prediction}")
         
         result = self._default_predictions(domain)
 
@@ -96,10 +117,11 @@ class LangChangePolicy(Policy):
             idx = domain.index_for_action(ACTION_LISTEN_NAME)
             result[idx] = 1.0
 
-        elif (previous_lang_record is not None and
-           all([lang_detect_above_threshold_Q(x) for x in [latest_lang_record, previous_lang_record]]) and
-           previous_lang_record.get('value') != latest_lang_record.get('value')):
-            
+        elif (
+            previous_lang_prediction is not None and
+            all([Q_lang_detect_above_threshold(x) for x in [latest_lang_prediction, previous_lang_prediction]]) and
+            previous_lang_prediction[0][0] != latest_lang_prediction[0][0]
+            ):
             idx = domain.index_for_action(self.fallback_action_name)
             if idx is None:
                 raise_warning(
@@ -113,24 +135,52 @@ class LangChangePolicy(Policy):
         return result
 
     def persist(self, path: Text) -> None:
-        """Only persists the priority."""
+        """
+        Persistes meta data and model once 
+        """
+    # if self.model:
 
-        config_file = os.path.join(path, "langchangepolicy.json")
+        meta_file = os.path.join(path, "langchangepolicy.json")
         meta = {"priority": self.priority,
                 "lang_detect_threshold": self.lang_detect_threshold,
-                "fallback_action_name": self.fallback_action_name
-                }
-        rasa.utils.io.create_directory_for_file(config_file)
-        rasa.utils.io.dump_obj_as_json_to_file(config_file, meta)
+                "fallback_action_name": self.fallback_action_name,
+                "model_path": self.model_path,
+            }
+        rasa.utils.io.create_directory_for_file(meta_file)
+        rasa.utils.io.dump_obj_as_json_to_file(meta_file, meta)
+
+        # model_file = os.path.join(path, "fastext_model.pkl")
+        # rasa.utils.io.pickle_dump(model_file, self._state)
+        # else:
+        #     raise_warning(
+        #         "Persist called without a model present. "
+        #         "Nothing to persist then!"
+        #     )
 
     @classmethod
     def load(cls, path: Text) -> "LangChangePolicy":
         """Returns the class with the configured priority."""
+        
+        
+        # if not os.path.exists(path):
+        #     raise OSError(
+        #         "Failed to load dialogue model. Path {} "
+        #         "doesn't exist".format(os.path.abspath(path))
+        #     )
 
         meta = {}
+#        state = {}
         if os.path.exists(path):
             meta_path = os.path.join(path, "langchangepolicy.json")
+            # model_file = os.path.join(path, "fastext_model.pkl")
             if os.path.isfile(meta_path):
                 meta = json.loads(rasa.utils.io.read_file(meta_path))
+            # if os.path.isfile(model_file):
+            #     state = rasa.utils.io.pickle_load(model_file)
+            #     logger.info("Loaded language detection model")
+
+        # policy = cls(**meta)
+
+        # vars(policy).update(state)
 
         return cls(**meta)
